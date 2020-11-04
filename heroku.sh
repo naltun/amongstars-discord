f3e3b23a2d16924c52243cc5f6bd0b6951ceba6f
chopin42 <julien.lejoly2712@gmail.com>
2020-11-05 00:09:10 +0100

    Adding inputs

--- heroku.sh
+++ heroku.sh
@@ -4,9 +4,11 @@ rm .gitignore
 git remote remove origin
 
 #heroku login
+echo "Please enter the name of the app (on heroku)"
 read NAME
 heroku git:remote -a $NAME
 
+echo "Please enter your token"
 read TOKEN
 echo "$TOKEN" > token.txt
 
